5d6e22423bc124ad4fa623a88e2e695990bc4e97
eliazara <meliiaazzahra54@gmail.com>
2025-11-15 11:34:50 +0700

    update index.html

---
+++ assets/script/scripts.js
@@ -0,0 +1,18 @@
+const contacts = [
+  {
+    id: 1,
+    fullName: "Mochamad Irvan",
+    phone: 62881080070700,
+    email: "mchmdirvan@example.com",
+    location: "Jakarta",
+  },
+  {
+    id: 2,
+    fullName: "Melia Az Zahra",
+    phone: 62881080080800,
+    email: "zmeliaa@example.com",
+    location: "Jakerta",
+  },
+];
+
+console.log(contacts);
