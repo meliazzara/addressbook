1c4619581fd33e8fac5b5b00015109bb84f15e13
eliazara <meliiaazzahra54@gmail.com>
2025-12-16 14:10:56 +0700

    fix render contacts full

--- assets/script/scripts.js
+++ assets/script/scripts.js
@@ -1,36 +1,50 @@
-const dataContacts = [
+let dataContacts = [
   {
     id: 1,
     fullName: "Arkhan Hibban Habibi",
-    phone: 62881080070700,
+    phone: "62881080070700",
     email: "hibbanhabibi@example.com",
     location: "Jakarta",
   },
   {
     id: 2,
     fullName: "Melia Az Zahra",
-    phone: 62881080080800,
+    phone: "62881080080800",
     email: "zmeliaa@example.com",
     location: "Jakarta",
   },
 ];
 
-function displayContacts() {
-  for (const contact of dataContacts) {
-    console.log(`
-🆔 : ${contact.id}
-🧑‍🦱 : ${contact.fullName}
-📱 : ${contact.phone}
-📍 : ${contact.location}
-✉️ : ${contact.email}
-`);
+function renderContacts(contacts = dataContacts) {
+  const tbody = document.getElementById("contactTableBody");
+  tbody.innerHTML = "";
+
+  if (contacts.length === 0) {
+    tbody.innerHTML = `
+      <tr>
+        <td colspan="5" class="empty">No contacts available</td>
+      </tr>
+    `;
+    return;
   }
+
+  contacts.forEach((contact) => {
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+      <td><input type="checkbox"></td>
+      <td>${contact.fullName}</td>
+      <td>${contact.email}</td>
+      <td>${contact.phone}</td>
+      <td>${contact.location}</td>
+    `;
+
+    tbody.appendChild(row);
+  });
 }
 
 function getLastId() {
-  if (dataContacts.length === 0) {
-    return 1;
-  }
+  if (dataContacts.length === 0) return 1;
   return dataContacts[dataContacts.length - 1].id + 1;
 }
 
@@ -42,31 +56,22 @@ function addContact(fullName, phone, email, location) {
     email,
     location,
   });
+
+  renderContacts();
 }
 
 function searchContacts(keyword) {
-  const filteredContacts = dataContacts.filter(
-    (contact) => contact.fullName === keyword
+  const result = dataContacts.filter((contact) =>
+    contact.fullName.toLowerCase().includes(keyword.toLowerCase())
   );
 
-  if (filteredContacts.length === 0) {
-    console.log("Data tidak ditemukan");
-    return;
-  }
-
-  for (const contact of filteredContacts) {
-    console.log(`
-🆔 : ${contact.id}
-🧑‍🦱 : ${contact.fullName}
-📱 : ${contact.phone}
-📍 : ${contact.location}
-✉️ : ${contact.email}
-`);
-  }
+  renderContacts(result);
 }
 
-// TEST
-addContact("Nam Jo Hyuk", 6281234567890, "hyuk@example.com", "Jakarta");
-addContact("Nam Jo Hyuk", 6281234567890, "hyuk@example.com", "Jakarta");
+document.addEventListener("DOMContentLoaded", () => {
+  renderContacts();
 
-searchContacts("Nam Jo Hyuk");
+  // TEST (BOLEH DIHAPUS)
+  addContact("Nam Jo Hyuk", "6281234567890", "hyuk@example.com", "Jakarta");
+  addContact("Nam Jo Hyuk", "6281234567890", "hyuk@example.com", "Jakarta");
+});
